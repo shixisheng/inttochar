188a7758c24129ee96909fee209f71557a6bd208
lqx <lqx0830@hotmail.com>
2011-11-15 15:03:09 +0800

    test

---
+++ wenjian/src/wenjianzhuanhuan/filetobite.java
@@ -0,0 +1,19 @@
+package wenjianzhuanhuan;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+public class filetobite {
+	public static void main(String args[]){
+		File f=new File("d:\\test.txt");
+		try{f.createNewFile();}catch(IOException e){e.printStackTrace();}
+		InputStream input=null;
+		try{input=new FileInputStream(f);}catch(IOException e){e.printStackTrace();}
+		byte b[]=new byte[1024];
+		try{input.read(b);}catch(IOException e){e.printStackTrace();}
+		try{input.close();}catch(IOException e){e.printStackTrace();}
+		System.out.println("内容为："+new String(b));
+	}
+   
+}
